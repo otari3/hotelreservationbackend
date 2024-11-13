3b875e09e969e0ae8aa59f0060c7140e1d141fcb
otari nozadze <otikonozadze@gmail.com>
2024-11-14 01:15:17 +0400

    added method error in users

--- hotelres/users/views.py
+++ hotelres/users/views.py
@@ -10,15 +10,18 @@ from reservation.models import Reservation
 
 @csrf_exempt
 def insert_user(request):
-  try:
-    data = json.loads(request.body)
-    hotel_id = getattr(request,'hotel_id',None)
-    user_id = User.insert_user(data,hotel_id['hotel_id'])
-    resvation = Reservation(check_in=data['check_in'],check_out=data['check_out'],user_id=user_id[0]['id'],  
-                              price=data['price'],in_hotel=False,nights=data['nights'], 
-                              hotel_id=hotel_id['hotel_id'],room_id=data['room_id'])
-    resvation.insert_reservation()
-    return JsonResponse({'user':user_id})
-  except Exception as e:
-    return erros.handel_errors(e,'/users/views/insert_user')
+  if request.method == 'POST':
+    try:
+      data = json.loads(request.body)
+      hotel_id = getattr(request,'hotel_id',None)
+      user_id = User.insert_user(data,hotel_id['hotel_id'])
+      resvation = Reservation(check_in=data['check_in'],check_out=data['check_out'],user_id=user_id[0]['id'],  
+                                price=data['price'],in_hotel=False,nights=data['nights'], 
+                                hotel_id=hotel_id['hotel_id'],room_id=data['room_id'])
+      resvation.insert_reservation()
+      return JsonResponse({'user':user_id})
+    except Exception as e:
+      return erros.handel_errors(e,'/users/views/insert_user')
+  else:
+    return JsonResponse({'error':f'method should be POST'},status=405)
   
