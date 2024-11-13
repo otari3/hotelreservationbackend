8928a2f4070a619710671c4ca0888f2f0dfe7f89
otari nozadze <otikonozadze@gmail.com>
2024-11-13 18:56:09 +0400

    made rservation model rdy

--- hotelres/hotelres/settings.py
+++ hotelres/hotelres/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hotels',
     'hotelrooms',
-    'users'
+    'users',
+    'reservation'
     
 ]
 
--- hotelres/reservation/models.py
+++ hotelres/reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
+from hotels.models import Hotels
+from hotelrooms.models import HotelRooms
 
 # Create your models here.
+
+class Reservation(models.Model):
+  check_in = models.DateField()
+  check_out = models.DateField()
+  user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+  price = models.IntegerField()
+  in_hotel = models.BooleanField()
+  nights = models.IntegerField()
+  hotel_id = models.ForeignKey(Hotels,on_delete=models.CASCADE)
+  room_id = models.ForeignKey(HotelRooms,on_delete=models.CASCADE)
+  class Meta:
+    unique_together = ('check_in','check_out','hotel_id','room_id')
