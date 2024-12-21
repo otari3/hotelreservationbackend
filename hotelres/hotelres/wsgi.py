4fe42dbaf0ae76aff746e0df9ec5136a4b6076fc
otari nozadze <otikonozadze@gmail.com>
2024-12-21 18:58:26 +0400

    tryiing again

--- hotelres/hotelres/wsgi.py
+++ hotelres/hotelres/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotelres.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotelres.hotelres.settings')
 
 application = get_wsgi_application()
